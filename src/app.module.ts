9d33e9305a48407ee198581960937c6793d1e96c
leonardo de la cruz <leonardodelacruz03@hotmail.com>
2023-10-23 10:24:30 -0500

    eslint fixed files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MunicipalitiesModule } from './municipalities/municipalities.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
-import { NeighborhoodsModule } from './neighborhoods/neighborhoods.module';
-import { SurveyedModule } from './surveyed/surveyed.module';
-import { VotingMunicipalitiesModule } from './voting-municipalities/voting-municipalities.module';
-import { PollingStationsModule } from './polling-stations/polling-stations.module';
-import { VotingTablesModule } from './voting-tables/voting-tables.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -26,15 +20,9 @@ dotenv.config();
       autoLoadEntities: true,
       synchronize: true,
     }),
-    MunicipalitiesModule,
     AuthModule,
     UsersModule,
     RolesModule,
-    NeighborhoodsModule,
-    SurveyedModule,
-    VotingMunicipalitiesModule,
-    PollingStationsModule,
-    VotingTablesModule,
   ],
 })
 export class AppModule {}
